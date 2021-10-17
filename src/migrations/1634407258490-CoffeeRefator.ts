0cc7c3854650c1e0da6de691f98d5bc6c17fcda0
Rachhen <rachhen.it@outlook.com>
2021-10-17 09:39:20 +0700

    typeorm migrations

---
+++ src/migrations/1634407258490-CoffeeRefator.ts
@@ -0,0 +1,15 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class CoffeeRefator1634407258490 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "coffee" RENAME COLUMN "name" TO "title"`,
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "coffee" RENAME COLUMN "title" TO "name"`,
+    );
+  }
+}
