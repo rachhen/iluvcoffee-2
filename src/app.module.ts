949a13af9c704ee748b600478b6d927b653f9f54
Rachhen <rachhen.it@outlook.com>
2021-10-17 13:38:56 +0700

    added guards

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoffeesModule } from './coffees/coffees.module';
 import { CoffeeRatingModule } from './coffee-rating/coffee-rating.module';
+import { CommonModule } from './common/common.module';
 import appConfig from './config/app.config';
 
 @Module({
@@ -31,6 +32,7 @@ import appConfig from './config/app.config';
     }),
     CoffeesModule,
     CoffeeRatingModule,
+    CommonModule,
   ],
   controllers: [AppController],
   providers: [AppService],
